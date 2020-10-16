629b135e35b20c22eb06dcef28d172aa17166222
Minho-Choi <2153373@naver.com>
2020-10-16 22:54:59 +0900

    Initial Commit

---
+++ BooBooBoo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BooBooBoo
+//
+//  Created by Minho Choi on 2020/10/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
